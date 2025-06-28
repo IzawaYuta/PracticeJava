c19ba17bae21c653042c5ad9eaf6833e6deba8e6
yutaizawa <yutamac2512@icloud.com>
2025-06-28 14:31:50 +0900

    forを使った計算

--- PracticeNo1/src/Calculation.java
+++ PracticeNo1/src/Calculation.java
@@ -1,11 +1,32 @@
 public class Calculation {
     public static void main(String[] args) {
+
+        String string1 = "改行は、";
+        String string2 = "しません。";
+
+        // System.out.print(string1);
+        // System.out.print(string2);
+
+        String string3 = "改行を、";
+        String string4 = "します。";
+
+        // System.out.println(string3);
+        // System.out.println(string4);
+
+        //基本的な計算
         int a = 5;
         int b = 2;
-        System.out.println(a + b); //足し算
-        System.out.println(a - b); //引き算
-        System.out.println(a * b); //掛け算
-        System.out.println(a / b); //割り算
-        System.out.println(a % b); //余り
+        // System.out.println(a + b); //足し算
+        // System.out.println(a - b); //引き算
+        // System.out.println(a * b); //掛け算
+        // System.out.println(a / b); //割り算
+        // System.out.println(a % b); //余り
+
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            sum += i; // sumにiを加算
+        }
+        System.out.println("1から10までの合計: " + sum);
     }
 }
