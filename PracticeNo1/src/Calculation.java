2c21f09cd4793704d70e468db4d50777bf3142d0
yutaizawa <yutamac2512@icloud.com>
2025-06-28 11:41:27 +0900

    基本的な計算：+ - * / %

---
+++ PracticeNo1/src/Calculation.java
@@ -0,0 +1,11 @@
+public class Calculation {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println(a + b); //足し算
+        System.out.println(a - b); //引き算
+        System.out.println(a * b); //掛け算
+        System.out.println(a / b); //割り算
+        System.out.println(a % b); //余り
+    }
+}
