7ba5de2fb60a78634ba254f51085c8feb040fdcc
yutaizawa <yutamac2512@icloud.com>
2025-07-01 12:19:17 +0900

    for繰り返し処理の基本

---
+++ PracticeNo1/src/forPrctice.java
@@ -0,0 +1,8 @@
+public class forPrctice {
+    public static void main(String[] args) {
+        int num = 0;
+        for(num = 0; num <= 10; num++) {
+            System.out.println(num);
+        }
+    }
+}
