6a84ba9ab32f356737de66d5d601d6c1c47ff656
yutaizawa <yutamac2512@icloud.com>
2025-07-01 09:30:37 +0900

    else ifの基本

--- PracticeNo1/src/Calculation.java
+++ PracticeNo1/src/Calculation.java
@@ -26,7 +26,7 @@ public class Calculation {
         int sum = 0;
 
         for (int i = 1; i <= 10; i++) {
-            sum += i; 
+            sum += i;
         }
         // System.out.println("1から10までの合計: " + sum);
 
@@ -37,6 +37,14 @@ public class Calculation {
             // System.out.println(num + "は正の数です");
         } else {
             // System.out.println(num + "は負の数です");
+        }
+
+        if (num == 1) {
+            System.out.println("one");
+        } else if (num == 2) {
+            System.out.println("two");
+        } else if (num == 3) {
+            System.out.println("three");
         }
     }
 }
