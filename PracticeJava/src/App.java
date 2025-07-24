a7fc77c0c73235fbc6800910914d0e9959ba3e53
yutaizawa <yutamac2512@icloud.com>
2025-07-24 21:53:58 +0900

    色々な数値を表示してみる

--- PracticeJava/src/App.java
+++ PracticeJava/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(10);
+        System.out.println(10); //整数
+        System.out.println(-8); //負の数
+        System.out.println(2.5); //小数点
+        System.out.println(1_000_000); //数値が大きいときに_を入れると見やすくなる。デバッグでは無視してくれる
+        System.out.println(1.2e3); //1.2に10の３条をかけたもの → 1.2*1000 = 12000
+        System.out.println(1.2e-3); //1.2に10の-3条をかけたもの = 0.0012
     }
 }
