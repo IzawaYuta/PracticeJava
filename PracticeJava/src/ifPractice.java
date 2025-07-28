280a29d79399bff0c7b2cb190304500ec903d580
yutaizawa <yutamac2512@icloud.com>
2025-07-28 15:20:49 +0900

    準備：入力状態を準備

---
+++ PracticeJava/src/ifPractice.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ifPractice {
+    public static void main(String[] args) {
+        int num = new Scanner(System.in) .nextInt();
+        System.out.println(num);
+    }
+}
